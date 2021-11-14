581d0eebfbde358029391a91c2bde30cb91b31b9
Dennis Jimenez <alejimenezc@hotmail.com>
2021-11-14 00:07:37 -0600

    Primeras pruebas con sockets

--- Server/main.c
+++ Server/main.c
@@ -1,4 +1,19 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+
 #include <stdio.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "WS2_32.lib")
+
+#define BUFLEN 512
+#define PORT 27015
+#define ADDRESS "127.0.0.1"
 
 int main() {
     printf("Hello, World!\n");
