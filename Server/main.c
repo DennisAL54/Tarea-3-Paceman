d55fdd2173e50c9e732a8c29bdeaca8c95e601ba
Dennis Jimenez <alejimenezc@hotmail.com>
2021-11-17 03:21:46 -0600

    Modificacion al server

--- Server/main.c
+++ Server/main.c
@@ -1,9 +1,11 @@
 /*
 	Live Server on port 8888
 */
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
 #include<io.h>
 #include<stdio.h>
 #include<winsock2.h>
+#define PORT 1000
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
@@ -13,10 +15,9 @@ int main(int argc , char *argv[])
     SOCKET s , new_socket;
     struct sockaddr_in server , client;
     int c;
-    char *message, reply[2000];
-    int recv_size;
 
     printf("\nInitialising Winsock...");
+
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
         printf("Failed. Error Code : %d",WSAGetLastError());
@@ -35,8 +36,8 @@ int main(int argc , char *argv[])
 
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_port = htons(PORT);
 
     //Bind
     if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
@@ -54,26 +55,45 @@ int main(int argc , char *argv[])
     puts("Waiting for incoming connections...");
 
     c = sizeof(struct sockaddr_in);
-
-    while( (new_socket = accept(s , (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
-    {
-        if((recv_size = recv(s , reply , 2000 , 0)) == SOCKET_ERROR)
-        {
-            puts("recv failed");
-        }
-
-        puts("Reply received\n");
-
-        //Add a NULL terminating character to make it a proper string before printing
-        reply[recv_size] = '\0';
-        puts(reply);
-        puts("Connection accepted");
-    }
-
+    new_socket = accept(s , (struct sockaddr *)&client, &c);
     if (new_socket == INVALID_SOCKET)
     {
         printf("accept failed with error code : %d" , WSAGetLastError());
         return 1;
+    }
+    int size = 1024;
+    char buff[size];
+    char sbuff[size];
+    int n;
+    int reader;
+    memset(buff, 0, size);
+    memset(sbuff, 0, size);
+    for(;;){
+        reader = recv(c, buff, 1024 * sizeof(char), 0);
+        if (reader == -1) {
+            perror("recv()");
+            break;
+        } else if (reader == 0) {
+            break;
+        } else {
+            // print buffer which contains the client contents
+            printf("From client: %s\t To client : ", buff);
+            // if msg contains "Exit" then server exit and chat ended.
+            if (strncmp("exit", buff, 4) == 0) {
+                printf("Server Exit...\n");
+                break;
+            }
+            bzero(buff, size);
+            n = 0;
+            // copy server message in the buffer
+            while ((sbuff[n++] = getchar()) != '\n');
+
+// and send that buffer to client
+            write(c, sbuff, sizeof(sbuff));
+            bzero(sbuff,size);
+
+        }
+
     }
 
     closesocket(s);
--- src/sockets/connection/Connection.java
+++ src/sockets/connection/Connection.java
@@ -2,17 +2,14 @@ package sockets.connection;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Connection {
-    private final int PUERTO = 8888; //Puerto para la conexión
-    private final String HOST = "localhost"; //Host para la conexión
     protected Socket cs; //Socket del cliente
     protected DataOutputStream salidaServidor; //Flujo de datos de salida
 
     public Connection(String tipo) throws IOException //Constructor
     {
-        cs = new Socket(HOST, PUERTO); //Socket para el cliente en localhost en puerto 8888
+        cs = new Socket("127.0.0.1", 1000); //Socket para el cliente en localhost en puerto 8888
     }
 }
